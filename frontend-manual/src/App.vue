d7e1a449b2c1658cf3cae39be0fbde018a4c33ce
Daniel Correa <yo@danielgara.com>
2021-08-21 12:25:50 -0500

    fix: margin

--- frontend-manual/src/App.vue
+++ frontend-manual/src/App.vue
@@ -19,11 +19,11 @@
       </div>
     </nav>
     <!-- Header -->
-    <div class="container mt-3">
+    <div class="container mt-4">
       <router-view/>
     </div>
     <!-- Footer -->
-    <footer class="text-center text-lg-start bg-light text-muted mt-3">
+    <footer class="text-center text-lg-start bg-light text-muted mt-4">
       <div class="text-center p-4" style="background-color: gainsboro;">
         © 2021 Copyright -
         <a class="text-reset fw-bold text-decoration-none" target="_blank" href="https://twitter.com/greglim81">Greg Lim</a> -
