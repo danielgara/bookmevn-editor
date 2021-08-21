e9c9f46ecd4dcf788da05f40e8ac5749a997de60
Daniel Correa <yo@danielgara.com>
2021-08-21 10:41:22 -0500

    chapter three about page

--- frontend-manual/src/App.vue
+++ frontend-manual/src/App.vue
@@ -3,7 +3,7 @@
     <!-- Header -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#">Movies APP</a>
+        <router-link to="/" class="navbar-brand">Movies APP</router-link>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
@@ -11,7 +11,7 @@
           <div class="navbar-nav">
             <a class="nav-link active" href="#">Home</a>
             <a class="nav-link active" href="#">List Movies</a>
-            <a class="nav-link active" href="#">About</a>
+            <router-link to="/about" class="nav-link active">About</router-link>
           </div>
         </div>
       </div>
--- frontend-manual/src/views/Home.vue
+++ frontend-manual/src/views/Home.vue
@@ -1,14 +1,12 @@
 <template>
-  <div class="home">
-    <div class="card">
-      <div class="card-header">
-        Movies
-      </div>
-      <div class="card-body">
-        <h5 class="card-title">Movies Application</h5>
-        <p class="card-text">Welcome to the Movies Application</p>
-        <img src="https://images.pexels.com/photos/3137890/pexels-photo-3137890.jpeg" class="img-fluid">
-      </div>
+  <div class="card">
+    <div class="card-header">
+      Movies
+    </div>
+    <div class="card-body">
+      <h5 class="card-title">Movies Application</h5>
+      <p class="card-text">Welcome to the Movies Application</p>
+      <img src="https://images.pexels.com/photos/3137890/pexels-photo-3137890.jpeg" class="img-fluid">
     </div>
   </div>
 </template>
